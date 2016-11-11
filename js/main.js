0eeef771f9316df1948f5f2428d093dfbcdf3920
Oliver Sherouse <oliver.sherouse@gmail.com>
2016-11-11 14:48:15 -0500

    site redesign

--- js/main.js
+++ js/main.js
@@ -1,49 +1,45 @@
-$(document).ready(function() {
-    var menuToggle = $("#js-navigation-centered-mobile-menu").unbind();
-    $("#js-navigation-centered-menu").removeClass("show");
-
-    menuToggle.on("click", function(e) {
-        e.preventDefault();
-        $("#js-navigation-centered-menu").slideToggle(function(){
-            if($("#js-navigation-centered-menu").is(":hidden")) {
-                $("#js-navigation-centered-menu").removeAttr("style");
-            }
+$(function() {
+    $("#nav-expander").find('a').click(function(){
+        $("#nav-links").slideToggle({
+            complete: function(){
+                $(this).toggleClass('hide');
+                $(this).removeAttr('style');
+            },
         });
     });
-});
-
-// Vertical Tabs
-$(".js-vertical-tab-content").hide();
-$(".js-vertical-tab-content:first").show();
-$(".js-vertical-tab:first").addClass("is-active");
-$(".js-vertical-tab-accordion-heading:first").addClass("is-active");
-
-/* if in tab mode */
-$(".js-vertical-tab").click(function(event) {
-    event.preventDefault();
-
+    // Vertical Tabs
     $(".js-vertical-tab-content").hide();
-    var activeTab = $(this).attr("rel");
-    $("#"+activeTab).show();
+    $(".js-vertical-tab-content:first").show();
+    $(".js-vertical-tab:first").addClass("is-active");
+    $(".js-vertical-tab-accordion-heading:first").addClass("is-active");
 
-    $(".js-vertical-tab").removeClass("is-active");
-    $(this).addClass("is-active");
+    /* if in tab mode */
+    $(".js-vertical-tab").click(function(event) {
+        event.preventDefault();
 
-    $(".js-vertical-tab-accordion-heading").removeClass("is-active");
-    $(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
-});
+        $(".js-vertical-tab-content").hide();
+        var activeTab = $(this).attr("rel");
+        $("#"+activeTab).show();
 
-/* if in accordion mode */
-$(".js-vertical-tab-accordion-heading").click(function(event) {
-    event.preventDefault();
+        $(".js-vertical-tab").removeClass("is-active");
+        $(this).addClass("is-active");
 
-    $(".js-vertical-tab-content").hide();
-    var accordion_activeTab = $(this).attr("rel");
-    $("#"+accordion_activeTab).show();
+        $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+        $(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
+    });
 
-    $(".js-vertical-tab-accordion-heading").removeClass("is-active");
-    $(this).addClass("is-active");
+    /* if in accordion mode */
+    $(".js-vertical-tab-accordion-heading").click(function(event) {
+        event.preventDefault();
 
-    $(".js-vertical-tab").removeClass("is-active");
-    $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+        $(".js-vertical-tab-content").hide();
+        var accordion_activeTab = $(this).attr("rel");
+        $("#"+accordion_activeTab).show();
+
+        $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+        $(this).addClass("is-active");
+
+        $(".js-vertical-tab").removeClass("is-active");
+        $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+    });
 });
