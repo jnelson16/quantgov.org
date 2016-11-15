19f5ea32402de8846310c7b8b20e36a8f6717208
Oliver Sherouse <oliver.sherouse@gmail.com>
2016-11-15 16:08:57 -0500

    redesign midnight regs tracker

--- _posts/2016-11-08-midnight_reg_tracker.markdown
+++ _posts/2016-11-08-midnight_reg_tracker.markdown
@@ -1,7 +1,7 @@
 ---
 layout: post
 pullchart: |
-    <div id="midnight_regs_container">
+    <div id="midnight_regs_chart">
 
     </div>
 
@@ -13,11 +13,17 @@ title: Live Midnight Regulations Tracker
 permalink: /midnight_regulation_tracker/
 ...
 
-{{page.pullchart}}
+<div id="midnight_regs_chart"></div>
 
-### How This Chart Works
 
-This chart uses the [Federal Register](http://federalregister.gov) API to search for midnight regulations every time the page is loaded. For this chart, a "Midnight Regulation" is defined as a rule listed under a Regulation ID Number (RIN) for which a proposed rule was published between June 1 of the election year and Inauguration Day, and a final rule was published from the day after Election Day through Inauguration Day.
+### Tracking Midnight Regulations
 
-For more on Midnight Regulations, see [more research from the Mercatus Center here.](https://www.mercatus.org/tags/midnight-regulations) 
+Under the United States Constitution, the president is elected in November, but does not take office until January. Meanwhile, the outgoing president’s administration can still use the regulatory rulemaking progress to enact new regulations. In the period between Election Day and Inauguration Day, agencies tend to publish a surge of new rules, either to preempt an anticipated change in policy from a new administration, or simply to take advantage of decreased political pressure. These rules, called "midnight regulations," are sometimes too unpopular to adopt before the election. Others are accompanied by seriously poor assessment of the need for the regulation or the benefits and costs of alternatives.
 
+The chart above shows the number of midnight regulations from the Clinton and Bush administrations, as well as live-updates with the number of midnight regulations from the current midnight period. The searchable table below live-updates with every midnight regulation deemed significant, meaning that it is likely to have an annual economic effect of \$100 million or more, interact adversely with the policies of other agencies, make substantial changes to entitlements, grants, user fees, or loan programs, or affect the president's policy priorities. Click on the title of any midnight regulation to be taken to its page at the *Federal Register*.
+
+#### Significant Midnight Regulations
+
+<table id="midnight_regs_table"> </table>
+<script src="/js/applets/midnight_regs.js">
+</script>
--- js/applets/midnight_regs.js
+++ js/applets/midnight_regs.js
@@ -1,156 +1,54 @@
 var midnight_reg_tracker = {};
 $(function(){
     midnight_reg_tracker.presidents = [{
-        name: 'Bill Clinton, 1st Term',
-        midnights_nonsig: 56,
-        midnights_sig: 15,
-        ready: true,
+        name: 'Bill Clinton',
+        sig: 112,
+        nonsig: 857,
     }, {
-        name: 'Bill Clinton, 2nd Term',
-        midnights_nonsig: 63,
-        midnights_sig: 14,
-        ready: true,
+        name: 'George W. Bush',
+        sig: 171,
+        nonsig: 854,
     }, {
-        name: 'George W. Bush, 1st Term',
-        midnights_nonsig: 47,
-        midnights_sig: 18,
-        ready: true,
-    }, {
-        name: 'George W. Bush, 2nd Term',
-        midnights_nonsig: 74,
-        midnights_sig: 30,
-        ready: true,
-    }, {
-        name: 'Barack Obama, 1st Term',
-        midnights_nonsig: 49,
-        midnights_sig: 13,
-        ready: true,
-    }, {
-        name: 'Barack Obama, 2nd Term',
+        name: 'Barack Obama',
         year: 2016,
         election_day: 8,
-        possibles: [],
-        midnights_nonsig_rins: [],
-        midnights_sig_rins: [],
-        midnights_nonsig: null,
-        midnights_sig: null,
-        ready: false,
+        sig: null,
+        nonsig: null,
     }];
 
-    midnight_reg_tracker.get_possible_rules = function(president, page) {
-        if (page === undefined) {
-            page = 1;
-        }
-        if (page == 1) {
-            console.log("Processing " + president.name);
+    midnight_reg_tracker.get_rule_counts = function(president, significance){
+        var significant = 0;
+        if (significance === 'sig'){
+            significant = 1;
         }
         $.ajax('https://www.federalregister.gov/api/v1/documents.json', {
                 data: {
-                    'per_page': 1000,
-                    'page': page,
-                    'fields[]': ['regulation_id_numbers'],
-                    'conditions[type]': 'PRORULE',
-                    'conditions[publication_date][gte]': president.year + '-06-01',
-                    'conditions[publication_date][lte]': (president.year + 1) + '-01-20'
-                },
-                dataType: 'jsonp',
-                cache: president.cache,
-                error: function(){
-                    console.log('Retrying ' + president.name + ', page ' + page);
-                    setTimeout(function(){
-                        midnight_reg_tracker.get_possible_rules(president, page);
-                    }, 2000);
-                },
-                success: function(data) {
-                    var reg;
-                    for (var i = 0; i < data.results.length; i++) {
-                        reg = data.results[i].regulation_id_numbers[0];
-                        if (reg === undefined){
-                            continue;
-                        }
-                        if ($.inArray(reg, president.possibilities) == -1){
-                            president.possibles.push(reg);
-                        }
-
-
-                    }
-                    var total_pages = data.total_pages || 1;
-                    if (page != total_pages) {
-                        midnight_reg_tracker.get_possible_rules(president, page + 1);
-                    } else {
-                        midnight_reg_tracker.identify_midnights(president);
-                    }
-                }
-    });
-    }
-    midnight_reg_tracker.identify_midnights = function(president, page) {
-        if (page === undefined) {
-            page = 1;
-        }
-        $.ajax('https://www.federalregister.gov/api/v1/documents.json', {
-                data: {
-                    'per_page': 1000,
-                    'fields[]': ['regulation_id_numbers', 'significant'],
-                    'page': page,
+                    'fields[]': 'type',
+                    'per_page': 2,
                     'conditions[type]': 'RULE',
+                    'conditions[significant]': significant,
                     'conditions[publication_date][gte]': president.year + '-11-0' + (president.election_day + 1),
                     'conditions[publication_date][lte]': (president.year + 1) + '-01-20'
                 },
                 dataType: 'jsonp',
-                cache: president.cache,
-                error: function(data){
-                    console.log('Retrying ' + president.name + ', page ' + page);
-                    setTimeout(function(){
-                        midnight_reg_tracker.identify_midnights(president, page);
-                    },
-                    2000);
-                },
-                success: function(data) {
-                    var reg;
-                    if (data.count == 0) {
-                        data.results = [];
+                success: function (data){
+                    console.log("Finished with " + significance);
+                    president[significance] = data['count'];
+                    if (president['sig'] === null || president['nonsig'] === null){
+                        return;
                     }
-                    for (var i = 0; i < data.results.length; i++) {
-                        reg = data.results[i];
-                        rin = reg['regulation_id_numbers'][0]
-                        if (rin === undefined){
-                            continue;
-                        }
-                        if (reg.significant){
-                            if ($.inArray(rin, president.midnights_sig_rins) == -1 && $.inArray(rin, president.possibles) != -1) {
-                                president.midnights_sig_rins.push(rin);
-                            }
-                        } else {
-                            if ($.inArray(rin, president.midnights_nonsig_rins) == -1 && $.inArray(rin, president.possibles) != -1) {
-                                president.midnights_nonsig_rins.push(rin);
-                            }
-                        }
-                    }
-                    var total_pages = data.total_pages || 1;
-                    if (page != total_pages) {
-                        midnight_reg_tracker.identify_midnights(idx, page + 1);
-                    } else {
-                        console.log("Done with " + president.name);
-                        president.midnights_sig = president.midnights_sig_rins.length;
-                        president.midnights_nonsig = president.midnights_nonsig_rins.length;
-                        president.ready = true;
-                        for (var i = 0; i < midnight_reg_tracker.presidents.length; i++){
-                            if (!midnight_reg_tracker.presidents[i].ready){
-                                return;
-                            }
-                        }
-                        midnight_reg_tracker.display();
-                    }
+                    midnight_reg_tracker.display();
                 }
-        });
+                });
+
     }
-    
+
     midnight_reg_tracker.display = function(){
-        var nonsig = [];
         var sig = [];
+        var nonsig = [];
         for (var i = 0; i < midnight_reg_tracker.presidents.length; i++){
-            sig.push(midnight_reg_tracker.presidents[i].midnights_sig);
-            nonsig.push(midnight_reg_tracker.presidents[i].midnights_nonsig);
+            sig.push(midnight_reg_tracker.presidents[i].sig);
+            nonsig.push(midnight_reg_tracker.presidents[i].nonsig);
         }
 
         midnight_reg_tracker.chart.series[0].setData(nonsig);
@@ -164,19 +62,59 @@ $(function(){
 
     };
 
-    var president;
-    var categories = [];
-    for (var i = 0; i < midnight_reg_tracker.presidents.length; i++){
-        president = midnight_reg_tracker.presidents[i];
-        if (!president.ready){
-            midnight_reg_tracker.get_possible_rules(president);
-        }
-        categories.push(president.name);
-    }
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var today = new Date();
+    midnight_reg_tracker.fill_table = function(president){
+        $.ajax('https://www.federalregister.gov/api/v1/documents.json', {
+                data: {
+                    'fields[]': ['publication_date', 'title', 'html_url', 'agency_names', 'topics'],
+                    'per_page': 10,
+                    'conditions[significant]': 1,
+                    'conditions[type]': 'RULE',
+                    'conditions[publication_date][gte]': president.year + '-11-0' + (president.election_day + 1),
+                    'conditions[publication_date][lte]': (president.year + 1) + '-01-20'
+                },
+                dataType: 'jsonp',
+                success: function(data){
+                    var rows = [];
+                    for (var i = 0; i < data.results.length; i++){
+                        var reg = data.results[i];
+                        rows.push([reg.publication_date, '<a href="' + reg.html_url + '" target="_blank">' + reg.title + '</a>', reg.agency_names.join(', '), reg.topics.join(', ')])
+                    }
+                midnight_reg_tracker.table = $('#midnight_regs_table').DataTable({
+                    data: rows,
+                    columns: [
+                        {title: 'Date'},
+                        {title: 'Rule'},
+                        {title: 'Agencies'},
+                        {title: 'Topics'},
+                    ]
+                });
+                },
+                error: function(problem){
+                    console.log(problem);
+                }
+                });
+    };
 
-    midnight_reg_tracker.chart = Highcharts.chart("midnight_regs_container", {
+
+    var container = $('#midnight_regs_container');
+    if (container.length !== 0){
+        container.append('<div id="midnight_regs_chart"></div><table id="midnight_regs_table"></table>');
+    }
+
+    var table = $('#midnight_regs_table');
+    if (!table.length !== 0){
+        midnight_reg_tracker.fill_table(midnight_reg_tracker.presidents[midnight_reg_tracker.presidents.length - 1]);
+    }
+    var chart = $('#midnight_regs_chart');
+    if (!table.length !== 0){
+        var categories = [];
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var today = new Date();
+        for (var i = 0; i < midnight_reg_tracker.presidents.length; i++){
+            categories.push(midnight_reg_tracker.presidents[i].name);
+        }
+
+        midnight_reg_tracker.chart = Highcharts.chart("midnight_regs_chart", {
             chart: {type: 'column'},
             title: {text: 'Midnight Rules by President'},
             subtitle: {text: 'as of ' + today.getDate() + ' ' + months[today.getMonth()] + ' ' + today.getFullYear()},
@@ -193,10 +131,16 @@ $(function(){
                 series: {stacking: 'normal'}
             },
             series: [
-                {name: "Non-Significant Midnight Rules", data: [0, 0, 0, 0, 0, 0]},
-                {name: "Significant Midnight Rules", data: [0, 0, 0, 0, 0, 0]},
+            {name: "Non-Significant Midnight Rules", data: [0, 0, 0]},
+            {name: "Significant Midnight Rules", data: [0, 0, 0]},
             ],
-            exporting: {enabled: false},
+            exporting: {
+                enabled: false,
+                filename: 'quantgov_midnight_regulations_tracker_' + today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + (today.getDate() + 1)).slice(-2),
+            },
         });
-    midnight_reg_tracker.chart.showLoading();
+        midnight_reg_tracker.chart.showLoading();
+        midnight_reg_tracker.get_rule_counts(midnight_reg_tracker.presidents[midnight_reg_tracker.presidents.length - 1], 'sig');
+        midnight_reg_tracker.get_rule_counts(midnight_reg_tracker.presidents[midnight_reg_tracker.presidents.length - 1], 'nonsig');
+    }
 });
